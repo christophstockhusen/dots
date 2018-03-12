1ee8366c3ee4f7ed7709d0d0be89f5f33dfce45e
Christoph Stockhusen <christoph.stockhusen@ottogroup.com>
2018-03-12 15:59:50 +0100

    worked on dots

--- src/dots/core.clj
+++ src/dots/core.clj
@@ -1,4 +1,5 @@
 (ns dots.core
+  (:gen-class)
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
@@ -9,17 +10,46 @@
    [59 134 134]
    [11 72 107]])
 
+(def sizes-and-counts
+  [{:size 2000 :count 10}
+   {:size 1000 :count 20}
+   {:size 500 :count 100}
+   {:size 100 :count 1000}])
+
+(defn circles-collide?
+  [circle1 circle2]
+  (let [[x1 y1 d1 _] circle1
+        [x2 y2 d2 _] circle2]
+    (> (+ (/ d1 2) (/ d2 2))
+       (Math/sqrt (+ (Math/pow (- x1 x2) 2)
+                     (Math/pow (- y1 y2) 2))))))
+
+(defn random-circle
+  [size]
+  [(rand-int 10000) (rand-int 10000) size (rand-nth colors)])
+
 (defn add-circle
-  [circle-list]
-  (conj circle-list [(rand-int 1000) (rand-int 1000) 100 (rand-nth colors)]))
+  [circle-list circle]
+  (if (every? #(not (circles-collide? circle %)) circle-list)
+    (conj circle-list circle)
+    circle-list))
+
+(defn add-circles
+  [circles final-circles]
+  (reduce add-circle final-circles circles))
+
+(defn random-circle-list
+  [n size]
+  (map random-circle (repeat n size)))
 
 (defn compute-circles
-  []
-  (loop [n 100
+  [sizes-and-counts]
+  (loop [sizes-and-counts sizes-and-counts
          circles []]
-    (if (zero? n)
+    (if (empty? sizes-and-counts)
       circles
-      (recur (dec n) (add-circle circles)))))
+      (recur (rest sizes-and-counts) 
+        (add-circles (random-circle-list (:count (first sizes-and-counts)) (:size (first sizes-and-counts))) circles)))))
 
 (defn draw-circle
   [[x y d [r g b]]]
@@ -32,16 +62,19 @@
   (q/background 240)
   (q/color-mode :rgb)
   (q/no-stroke)
-  (compute-circles))
+  (compute-circles sizes-and-counts))
 
 (defn draw-state [state]
   (let [circles state]
-    (doall (map draw-circle circles))))
+    (doall (map draw-circle circles)))
+    (q/save "test.png"))
 
-(q/defsketch hello-quil
-  :title "You spin my circle right round"
-  :size [1000 1000]
-  :setup setup
-  :draw draw-state
-  :features [:keep-on-top]
-  :middleware [m/fun-mode])
+(defn -main
+  []
+  (q/sketch
+   :title "dots"
+   :size [10000 10000]
+   :setup setup
+   :draw draw-state
+   :features [:keep-on-top :exit-on-close]
+   :middleware [m/fun-mode]))
